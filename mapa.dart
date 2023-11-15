4c03e9b388745319b1f2e75ddeeae6d80012ea38
emilia <emi1213galarza@gmail.com>
2023-11-15 00:01:09 -0500

    Complete variables.dart and create mapa.dart

---
+++ mapa.dart
@@ -0,0 +1,9 @@
+void main() {
+  final person = {
+    'name': 'Jairo',
+    'lastName': 'Garcia',
+    'age': 23,
+  };
+
+  print(person['name']);
+}
--- variables.dart
+++ variables.dart
@@ -1,5 +1,31 @@
 void main() {
   String name = 'Jairo';
   name = 'Juan';
-  print('Hola $name!');
+
+  //final -> no se puede cambiar el valor
+  final String lastName = 'Garcia';
+
+  //null safety -> ?
+  final bool? isAlive = null;
+  // Para imprimir una variable dentro de un String se usa $nombreVariable
+  print('Hola $name!  $lastName ');
+  // Si no existen las comillas de solo el nombre de la variable
+  print(isAlive);
+  //Si dentro de la variable se usa un metodo se usa ${nombreVariable.metodo}
+  print('Hola $name!  ${lastName.toUpperCase()} ');
+  //Para declarar el tipo del que sera un listado se puede usar:
+  final List<String> names = ['Juan', 'Pedro', 'Fernando'];
+  final lastNames = <String>['Garcia', 'Perez', 'Lopez'];
+
+  // dynamic -> puede ser cualquier tipo de dato y por defecto es null
+  dynamic otherName = 'Jairo';
+  otherName = 1;
+  otherName = true;
+  otherName = null;
+  otherName = ['Hola', 'Mundo'];
+  print(""" 
+    $names
+    $lastNames
+    $otherName
+  """);
 }
