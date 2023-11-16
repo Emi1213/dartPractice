c0d1c0091f31302523aae3d91499f64e729a076a
emilia <emi1213galarza@gmail.com>
2023-11-15 22:22:20 -0500

    Add file map and information about use map in dart

--- mapa.dart
+++ mapa.dart
@@ -5,5 +5,27 @@ void main() {
     'age': 23,
   };
 
-  print(person['name']);
+  // En dart como en java se resume a object -> Objetos
+  final address = {
+    'city': 'Bogota',
+    'country': 'Colombia',
+  };
+
+  // Dentro de un map se puede tener varios objetos como String, listados, mapas
+  final Map<String, dynamic> students = {
+    'name': 'Jairo',
+    'lastName': 'Garcia',
+    'abilities': <String>['Dart', 'Java', 'Python'],
+    'academic instruction': <String, dynamic>{
+      'high school': 'Colegio Santo Domingo de Guzman',
+      'university': 'Universidad Tecnica de Ambato'
+    }
+  };
+
+  print(person);
+  print(address);
+
+  // Acceder a un objeto dentro de un map
+  print('Habilidades: ${students['abilities'][0]}');
+  print('Institucion: ${students['academic instruction']['high school']}');
 }
