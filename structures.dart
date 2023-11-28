6c061f7182c3dd9978aaff75637c93ee6ca20748
emilia <emi1213galarza@gmail.com>
2023-11-28 11:57:31 -0500

    Add iterables set and list file

--- structures.dart
+++ structures.dart
@@ -2,5 +2,16 @@ void main() {
   final numbers = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 9, 10];
 
   print('List original $numbers ');
+  //to Set convierte la lista en un conjunto y elimina los duplicados
   print('List sin duplicados ${numbers.toSet()}');
+  //imprimir el primer elemento de la lista
+  print('First ${numbers.first}');
+  //imprimir la cantidad de elementos de la lista
+  print('Length ${numbers.length}');
+  //imprimir el ultimo elemento de la lista
+  print('Last ${numbers.last}');
+  //imprimir el elemento en la posicion 6 de la lista
+  print('ElementAt ${numbers.elementAt(6)}');
+  // imprimir un elemento con el index
+  print('Element ${numbers[0]}');
 }
